9ef26bc5b276dd084e3b25ddaa5ee900332dcbb7
teobler <teobler@163.com>
2023-05-24 10:59:19 +0800

    fix storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,32 @@
-import type { StorybookConfig } from "@storybook/react-webpack5";
+import type { StorybookConfig } from "@storybook/nextjs";
+
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
   ],
   framework: {
-    name: "@storybook/react-webpack5",
+    name: "@storybook/nextjs",
     options: {},
   },
   docs: {
     autodocs: "tag",
+  },
+  webpackFinal: async (config, previewAnnotations) => {
+    // disable whatever is already set to load SVGs
+    config.module.rules
+      .filter((rule) => (rule["test"] && rule["test"].test(".svg")))
+      .forEach((rule) => (rule["exclude"] = /\.svg$/i));
+
+    // Configure .svg files to be loaded with @svgr/webpack
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack", "url-loader"],
+    });
+
+    return config;
   },
 };
 export default config;
