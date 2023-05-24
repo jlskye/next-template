d73737a8ed0a6eceae48a2e40ec57cafbab8799f
teobler <teobler@163.com>
2023-05-24 14:02:44 +0800

    remove useless dependencies and fix error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
 
 import ErrorBoundary from "src/components/ErrorBoundary";
-import AnalyticsHandler from "src/analytics";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <ErrorBoundary>
-      <AnalyticsHandler />
-
-      {/*<UIProvider>*/}
-      {/*  <Layout>*/}
-          <Component {...pageProps} />
-        {/*</Layout>*/}
-      {/*</UIProvider>*/}
+      <Component {...pageProps} />
     </ErrorBoundary>
-  )
+  );
 }
 
-export default App
+export default App;
