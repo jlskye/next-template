f6c5816c320c9cab04cf4609fcb19d520ed2b6b1
teobler <teobler@163.com>
2023-05-25 15:32:19 +0800

    migrate storybook from webpack to vite

--- packages/ui-components/.storybook/main.ts
+++ packages/ui-components/.storybook/main.ts
@@ -1,29 +1,20 @@
-import type { StorybookConfig } from "@storybook/nextjs";
-// fix this
+import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/stories/**/*.mdx", "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
-  framework: {
-    name: "@storybook/nextjs",
-    options: {},
-  },
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm",
+  ],
+  core: {},
   docs: {
     autodocs: "tag",
   },
-  webpackFinal: async (config, previewAnnotations) => {
-    // disable whatever is already set to load SVGs
-    config.module.rules
-      .filter((rule) => rule["test"] && rule["test"].test(".svg"))
-      .forEach((rule) => (rule["exclude"] = /\.svg$/i));
-
-    // Configure .svg files to be loaded with @svgr/webpack
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack", "url-loader"],
-    });
-
-    return config;
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
   },
 };
 export default config;
