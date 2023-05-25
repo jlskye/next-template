3f354571bb94242caccac8d9a0a7c4b8996df077
teobler <teobler@163.com>
2023-05-25 15:15:25 +0800

    add e2e placeholder

--- packages/ui-components/.storybook/main.ts
+++ packages/ui-components/.storybook/main.ts
@@ -1,12 +1,9 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+// fix this
 
 const config: StorybookConfig = {
   stories: ["../src/stories/**/*.mdx", "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
   framework: {
     name: "@storybook/nextjs",
     options: {},
@@ -17,7 +14,7 @@ const config: StorybookConfig = {
   webpackFinal: async (config, previewAnnotations) => {
     // disable whatever is already set to load SVGs
     config.module.rules
-      .filter((rule) => (rule["test"] && rule["test"].test(".svg")))
+      .filter((rule) => rule["test"] && rule["test"].test(".svg"))
       .forEach((rule) => (rule["exclude"] = /\.svg$/i));
 
     // Configure .svg files to be loaded with @svgr/webpack
