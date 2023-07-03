74902226ed0b1044af7e5cebefd1699caf5790c1
teobler <teobler@163.com>
2023-07-03 15:13:43 +0800

    fix test

--- apps/web-app/src/__tests__/index.test.tsx
+++ apps/web-app/src/__tests__/index.test.tsx
@@ -1,13 +1,11 @@
-import Home from "src/pages/index";
-import { render, screen, within } from "@testing-library/react";
+import Home from "src/app/[lang]/page";
+import { render, screen } from "@testing-library/react";
 
 describe("index page", () => {
-  it("should render without crashing", () => {
-    render(<Home />);
-    const main = within(screen.getByRole("main"));
+  it("should render without crashing", async () => {
+    render(await Home());
+    const button = screen.getByRole("button");
 
-    expect(
-      main.getByRole("heading", { level: 1, name: /Hello World/i })
-    ).toBeDefined();
+    expect(button.innerText).toBe("test");
   });
 });
